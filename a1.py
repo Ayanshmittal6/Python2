ee505111f3a63f994863396b1c64224b17fe8c0c
Ayanshmittal6 <rajivmittal.mbamdu@gmail.com>
2026-06-08 18:32:19 +0530

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,2 +1,23 @@
-print("hello")
-print("AYANSH")
+print("My daily routine")
+
+print("7:00 AM – Wake Up & Stretch: Get out of bed and drink a glass of water.")
+
+print("6:15 AM – Daily Movement: Do light stretching, yoga, or a quick workout ")
+
+print("07:00 AM — Dress & Prep: Put on clothes, brush hair, and tidy the bed.")
+
+print("07:15 AM — Nutritious Breakfast: Fuel the brain with eggs, fruit, or oatmeal; avoid morning screens.")
+
+print("")
+
+print
+
+print
+
+print
+
+print
+
+print
+
+print
