e2b6f8e5e879c77875c39efbffab720c3644e37c
Ayanshmittal6 <rajivmittal.mbamdu@gmail.com>
2026-06-10 15:53:10 +0530

    Add files via upload

---
+++ c2.py
@@ -0,0 +1,9 @@
+a=input("type your name ")
+c=input("type your weight ")
+Name=a
+is_student=True
+weight=234234
+print("Name:",Name)
+print("data type Name is", type(Name))
+print("weight:",weight)
+print("data type Name is", type(weight))
