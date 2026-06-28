28779ca272cfe1aef3bd391174ca8451f6da18f2
Ayanshmittal6 <rajivmittal.mbamdu@gmail.com>
2026-06-28 10:37:09 +0530

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,13 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+
+#find the total trees
+sum=tree1+tree2+tree4+tree5+tree3
+print("the sum of all trees is=",sum)
+
+#the average of treessum)
+average=sum/5
+print("the average of all trees is=",average)
