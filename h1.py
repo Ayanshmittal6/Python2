17b404b311cf19a1311732a5cb9e1da6e53434fb
Ayanshmittal6 <rajivmittal.mbamdu@gmail.com>
2026-07-26 10:49:11 +0530

    Add files via upload

---
+++ h1.py
@@ -0,0 +1,12 @@
+x=10
+y=2
+w=4
+d=5
+h=0
+h=(x+y)*w/d
+print("Z=",h)
+Name =input("TYPE YOUR NAME")
+if (Name=="Ayansh" or "Rajiv" or "Mehak"):
+   print("welcome")
+else:
+    print("not in list")
---
+++ h2.py
@@ -0,0 +1,6 @@
+numn=int(input("type a number : "))
+numd=int(input("type a number : "))
+if numn%numd==0:
+    print("\n" +str(numn)+ "is divisible" +str(numd))
+else:
+    print("\n" +str(numn)+ "is not divisible" +str(numd))
