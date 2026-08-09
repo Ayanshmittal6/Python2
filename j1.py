ac1d9707730998f3378c75a1af7f044b4be902f3
Ayanshmittal6 <rajivmittal.mbamdu@gmail.com>
2026-08-09 21:12:57 +0530

    Add files via upload

--- j1.py
+++ j1.py
@@ -1,6 +1,13 @@
-n = int(input("enter number whose sum you want to find"))
-sum=0
-
-for i in range(1, n+1):
-    sum = sum + i
-    print("\nsum=",sum)
+c = (input("enter / or **=")) 
+if (c == "**"):
+    x = int (input("enter number "))
+    y = int(input("enter power "))
+    a = 1
+    for i in range (y):
+        a=a*x
+    print("output is", a)
+else:
+      x = int (input("enter number 1 "))
+      y = int(input("enter number 2 "))
+      sum = x/y
+      print("output is ", sum)
