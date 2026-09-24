dc37a4d3bf18ee22219c322cf917b2e5e51ee73d
Ayanshmittal6 <rajivmittal.mbamdu@gmail.com>
2026-09-24 20:22:39 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,42 @@
+print("======================")
+print("#think of a number app")
+print("======================")
+print(" ")
+print("you will get 5 chances")
+print("the number will be from 20 to 70")
+print("❤️ ❤️ ❤️ ❤️ ❤️")
+a = int (input("enter the number you think"))
+if a == (67):
+    print("GOOD JOB YOU WIN")
+else:
+    print("I am giving you a clue")
+    print("it is a in seal a brainout")
+    print("❤️ ❤️ ❤️ ❤️")
+    a = int(input("enter the number you thing again"))
+    if a == (67):
+         print("YOU WIN GOOD JOB")
+    else:
+         print("I am giving you a clue")
+         print("it has its own song")
+         print("❤️ ❤️ ❤️")
+         a = int(input("enter the number you thing again"))
+         if a == (67):
+             print("YOU WIN GOOD JOB")
+         else:
+             print("I am giving you a clue")
+             print("when you type it in google the screen starts to shake")
+             print("❤️ ❤️")
+             a = int(input("enter the number you thing again"))
+             if a == (67):
+                    print("YOU WIN GOOD JOB")
+             else:
+                 print("I am giving you a clue")
+                 print("its 6+7")
+                 print("❤️")
+                 a = (input("enter the number you thing again"))
+                 if a == (67):
+                     print("YOU WIN GOOD JOB")
+                 else:
+                     print("YOU LOST")
+                     print("its 67")
+ 
